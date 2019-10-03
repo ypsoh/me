ce9ca019c09ab247ac488f4bdf0a603c6396fabd
Paul Soh <paulsoh@mbair.local>
2019-10-02 22:02:07 -0700

    Add about_me component for future use

---
+++ components/about_me.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <iframe
+    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsF-Xx6fE3j30Pgx4SghKPDVmpvw4IEEIw7oIcxAn_ImdKCCmIX6NL7ENToX7R_L2RuqTK3m5T5YXn/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" 
+    style={{
+      width: 800,
+      height: 600,
+      frameborder: 0,
+      scrolling: 'no'
+    }}
+  />
+)
